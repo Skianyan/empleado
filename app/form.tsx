465e84ec77267bd1ac2da64aefbb37dc9e62048c
Skianyan <99053383+Skianyan@users.noreply.github.com>
2023-05-29 23:41:24 -0700

    add color fix

--- app/form.tsx
+++ app/form.tsx
@@ -21,7 +21,7 @@ const Formulario = () => {
         <FormInput nombre={'Genero'} placeholder={'Genero'}/>
         <FormInput nombre={'Correo'} placeholder={'Correo'}/>
           
-          <button type='submit' className=' bg-blue-500 hover:bg-blue-700 text-white ml-56 font-bold py-2 px-8 mt-2 
+          <button type='submit' className=' bg-blue-500 hover:bg-blue-700 text-white ml-56 font-bold py-2 px-8 mt-2 mb-4 
           rounded-full'>Enviar</button>
         </form>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Formulario from './form'
 
 const HomePage = () => {
   return (
-    <main className='bg-[#e8e8e8] rounded-lg mx-4'>
+    <main className='bg-[#cfd8da] rounded-lg mx-4'>
       <header className='flex flex-row bg-[#67baee] p-4 mt-4 rounded-t-lg'>
         <div className="bg-slate-50 p-3 mx-2 rounded-full"></div>
         <div className="bg-slate-50 p-3 mx-2 rounded-full"></div>
         <div className="bg-slate-50 p-3 mx-2 rounded-full"></div>
       </header>
-    <section className='pb-5'>
+    <section className='pb-2'>
       <p className= 'text-center text-2xl uppercase font-bold text-gray-700 mt-3'>Empleados</p>
     </section>
     <Formulario />
