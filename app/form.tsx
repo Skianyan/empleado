54768a6fd9a5cc21e83de88bf46bdb2bcf8119ef
Skianyan <99053383+Skianyan@users.noreply.github.com>
2023-05-30 14:25:39 -0700

    yes

--- app/form.tsx
+++ app/form.tsx
@@ -4,13 +4,15 @@ type InputProps = {
     nombre:string,
     placeholder:string,
     setting:any
+    tipo:string
 }
 
-function FormInput ({nombre,placeholder,setting}:InputProps) {
+
+function FormInput ({nombre,placeholder,setting,tipo}:InputProps) {
     return(
         <div className='flex flex-col mx-4'>
             <label htmlFor='' className='ml-1'>{nombre}</label>
-            <input type="text" placeholder={placeholder}
+            <input type={tipo} placeholder={placeholder}
             onChange={(event)=> setting(event.target.value)} 
             className='bg-[#f8f8f8] focus:bg-blue-100 min-w-0 max-w-xs mb-1 pl-1 outline outline-1 rounded-lg'></input>
           </div>
@@ -32,10 +34,10 @@ const Formulario = () => {
     return(
     <div className='grid grid-cols-2'>
     <form onSubmit={formHandler} className='bg-slate-400 pt-2'>
-        <FormInput nombre={'Nombre'} placeholder={'Nombre'} setting={setNombre}/>
-        <FormInput nombre={'Edad'}   placeholder={'Edad'} setting={setEdad}/>
-        <FormInput nombre={'Genero'} placeholder={'Genero'} setting={setGenero}/>
-        <FormInput nombre={'Correo'} placeholder={'Correo'} setting={setCorreo}/>
+        <FormInput nombre={'Nombre'} placeholder={'Nombre'} setting={setNombre} tipo={'text'}/>
+        <FormInput nombre={'Edad'}   placeholder={'Edad'} setting={setEdad} tipo={'number'}/>
+        <FormInput nombre={'Genero'} placeholder={'Genero'} setting={setGenero} tipo={'text'}/>
+        <FormInput nombre={'Correo'} placeholder={'Correo'} setting={setCorreo} tipo={'email'}/>
           
         <button type='submit' className=' bg-blue-500 hover:bg-blue-700 text-white float-right font-bold py-2 px-8 mr-4 mt-2 mb-4 
         rounded-full'>Enviar</button>
